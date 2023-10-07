0264e828b237c744027bca423754d6ec8d8b6853
Lucas de Freitas Soares <lucas.soares00526525@sesisenaipr.org.br>
2023-10-07 16:01:52 -0300

    ?
    Algumas atualizações

---
+++ _js/index.js
@@ -0,0 +1,27 @@
+var dados = [ 
+    {nome: "Bicicleta Infantil"}, 
+    {nome: "Bicicleta Teen"}, 
+    {nome: "Bicicleta Adulta"}
+];
+
+
+for (var i = 0 ; i < dados.length ; i++ ) {
+
+var conteudo = 
+        `<div class="card">
+        <div class="imagem_produto">
+            <img src="_img/bicicleta.jpg" class="imagem_produto"/>
+        </div>
+        <div class="nome_produto">
+            <p class="nome_produto">${dados[i].nome}</p>
+        </div>
+        <div class="preco_produto">
+            <p class="preco_produto">R$500,00</p>
+        </div>
+        <div class="adicionar_carrinho">
+            <button type="button" class="adicionar_carrinho">Adicionar ao Carrinho</button>
+        </div>
+    </div>`;
+
+document.getElementById('aplicativos').innerHTML += conteudo;
+}
